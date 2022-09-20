e530179053910548d80b54d6f52928516658bd44
JasonXChen <jasonxchen888@gmail.com>
2022-09-20 13:28:39 -0400

    Add search by muscle or equipment

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -19,7 +19,21 @@ router.get("/", async (req, res) => {
             // String.prototype.match() doesn't work in IE
             // To do: RegExp.prototype.test() does, but doesn't return results that are prefixed or suffixed
             const search = new RegExp(req.query.q, "gi"),
-            filtered = results.filter(result => result.name.match(search));
+            filtered = results.filter(result => {
+                // keep results that have matching exercise name or muscle group
+                if (result.name.match(search)) {
+                    return true;
+                }
+                else if (result.category.name.match(search)) {
+                    return true;
+                }
+                // keep results that have matching equipment name
+                result.equipment.forEach(equipment => {
+                    if (equipment.name.match(search)) {
+                        return true;
+                    }
+                })
+            });
             // filter results to only contain english entries (built-in API url filter broken as of 2022-09-19)
             const exercises = filtered.filter(item => item.language.id === 2);
             res.render("exercises/index.ejs", {exercises, page});
